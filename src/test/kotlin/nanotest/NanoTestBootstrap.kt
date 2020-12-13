1377dff920f89533aec82b69a0f710d0e7aae446
Christoph Sturm <me@christophsturm.com>
2020-12-13 14:11:13 +0100

    extract to src/main

--- src/test/kotlin/nanotest/NanoTestBootstrap.kt
+++ src/test/kotlin/nanotest/NanoTestBootstrap.kt
@@ -43,30 +43,3 @@ fun main() {
 }
 
 
-class Suite {
-    val contexts = mutableListOf<TestContext>()
-    fun context(contextName: String, function: TestContext.() -> Unit) {
-        val testContext = TestContext()
-        contexts.add(testContext)
-        testContext.function()
-    }
-
-    fun awaitExecution() = SuiteResult(false, contexts.flatMap(TestContext::testFailures))
-}
-
-class TestContext {
-    val testFailures = mutableListOf<TestFailure>()
-
-    fun test(testName: String, function: () -> Unit) {
-        try {
-            function()
-        } catch (e: AssertionError) {
-            testFailures.add(TestFailure(testName, e))
-        }
-    }
-
-}
-
-data class SuiteResult(val allOk: Boolean, val failedTests: Collection<TestFailure>)
-
-data class TestFailure(val name: String, val throwable: Throwable)

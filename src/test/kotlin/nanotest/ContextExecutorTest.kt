42c146d3360a11945c09910873e062842e5288ce
Christoph Sturm <me@christophsturm.com>
2020-12-16 17:17:12 +0100

    fix test

--- src/test/kotlin/nanotest/ContextExecutorTest.kt
+++ src/test/kotlin/nanotest/ContextExecutorTest.kt
@@ -7,33 +7,37 @@ import strikt.assertions.isA
 
 object ContextExecutorTest {
     val context = Context {
-        val events = mutableListOf<String>()
-        val ctx = Context("root context") {
-            events.add("root context")
-            test("test 1") {
-                events.add("test 1")
-            }
-            test("test 2") {
-                events.add("test 2")
-            }
-            context("context 1") {
-                events.add("context 1")
+        test("context executor test") {
+            val events = mutableListOf<String>()
+            val ctx = Context("root context") {
+                events.add("root context")
+                test("test 1") {
+                    events.add("test 1")
+                }
+                test("test 2") {
+                    events.add("test 2")
+                }
+                context("context 1") {
+                    events.add("context 1")
 
-                context("context 2") {
-                    events.add("context 2")
-                    test("test 3") {
-                        events.add("test 3")
+                    context("context 2") {
+                        events.add("context 2")
+                        test("test 3") {
+                            events.add("test 3")
+                        }
                     }
                 }
+
             }
+            val result: List<TestResult> = ContextExecutor(ctx).execute()
+            expectThat(events).containsExactly(
+                "root context", "test 1",
+                "root context", "test 2",
+                "root context", "context 1", "context 2", "test 3"
+            )
+            expectThat(result).all { isA<Success>() }
+
         }
-        val result: List<TestResult> = ContextExecutor(ctx).execute()
-        expectThat(events).containsExactly(
-            "root context", "test 1",
-            "root context", "test 2",
-            "root context", "context 1", "context 2", "test 3"
-        )
-        expectThat(result).all { isA<Success>() }
     }
 }
 

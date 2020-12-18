4af2e56564df8ea8bce3fa89b339dae60246e92e
Christoph Sturm <me@christophsturm.com>
2020-12-18 12:41:23 +0100

    pimp the gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.20"
+    java
+    kotlin("jvm") version "1.4.21"
     id("com.github.ben-manes.versions") version "0.36.0"
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.5"
 }
 
 group = "nanotest"
-version = "1.0-SNAPSHOT"
+version = "0.1"
 
 repositories {
     mavenCentral()
@@ -16,8 +19,31 @@ dependencies {
     testImplementation("io.strikt:strikt-core:0.28.1")
 }
 
-
+tasks {
+    create<Jar>("sourceJar") {
+        from(sourceSets.main.get().allSource)
+        archiveClassifier.set("sources")
+    }
+}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+artifacts {
+    add("archives", tasks["jar"])
+    add("archives", tasks["sourceJar"])
+}
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(tasks["sourceJar"])
+            groupId = project.group as String
+            artifactId = "nanotest"
+            version = project.version as String
+        }
+    }
+}
+
+tasks.wrapper { distributionType = Wrapper.DistributionType.ALL }

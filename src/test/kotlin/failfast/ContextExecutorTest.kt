68d3373290fc8479c20e66ad7d4f775b7b0d23a0
christoph <me@christophsturm.com>
2020-12-28 18:08:22 +0100

    use coroutineScope instead of runBlocking

--- src/test/kotlin/failfast/ContextExecutorTest.kt
+++ src/test/kotlin/failfast/ContextExecutorTest.kt
@@ -1,10 +1,9 @@
 package failfast
 
 import failfast.internal.ContextExecutor
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.coroutineScope
 import strikt.api.expectThat
 import strikt.assertions.containsExactly
 import strikt.assertions.containsExactlyInAnyOrder
@@ -32,9 +31,9 @@ object ContextExecutorTest {
                 }
 
             }
-            val testResultChannel = Channel<TestResult>(UNLIMITED)
-            @Suppress("BlockingMethodInNonBlockingContext")
-            runBlocking(Dispatchers.Unconfined) {
+
+            coroutineScope {
+                val testResultChannel = Channel<TestResult>(UNLIMITED)
                 val rootContext = Context("root context", null)
                 val context1 = Context("context 1", rootContext)
                 val context2 = Context("context 2", context1)
@@ -42,13 +41,14 @@ object ContextExecutorTest {
                 val contextInfo = ContextExecutor(ctx, testResultChannel, this).execute()
                 expectThat(contextInfo.tests).isEqualTo(4)
                 expectThat(contextInfo.contexts).containsExactlyInAnyOrder(rootContext, context1, context2, context4)
+
+                // we expect 4 times success
+                expectThat(testResultChannel.receive()).isA<Success>()
+                expectThat(testResultChannel.receive()).isA<Success>()
+                expectThat(testResultChannel.receive()).isA<Success>()
+                expectThat(testResultChannel.receive()).isA<Success>()
             }
 
-            // we expect 4 times success
-            expectThat(testResultChannel.receive()).isA<Success>()
-            expectThat(testResultChannel.receive()).isA<Success>()
-            expectThat(testResultChannel.receive()).isA<Success>()
-            expectThat(testResultChannel.receive()).isA<Success>()
 
 
         }

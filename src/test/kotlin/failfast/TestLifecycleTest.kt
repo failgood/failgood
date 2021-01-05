a868298a1a7279402013aa91566aba9eb8963a05
christoph <me@christophsturm.com>
2021-01-05 23:00:19 +0100

    create enum for events

--- src/test/kotlin/failfast/TestLifecycleTest.kt
+++ src/test/kotlin/failfast/TestLifecycleTest.kt
@@ -1,5 +1,6 @@
 package failfast
 
+import failfast.Event.*
 import strikt.api.expectThat
 import strikt.assertions.containsExactlyInAnyOrder
 
@@ -7,53 +8,56 @@ fun main() {
     Suite(TestLifecycleTest.context).run().check()
 }
 
+enum class Event {
+    ROOT_CONTEXT_EXECUTED,
+    DEPENDENCY_CLOSED,
+    TEST_1_EXECUTED,
+    TEST_2_EXECUTED,
+    CONTEXT_1_EXECUTED,
+    CONTEXT_2_EXECUTED,
+    TEST_3_EXECUTED,
+    TEST_4_EXECUTED
+}
+
 object TestLifecycleTest {
-    const val ROOT_CONTEXT_EXECUTED = "root context executed"
-    const val DEPENDENCY_CLOSED = "autoClose callback called"
-    const val TEST_1_EXECUTED = "test 1 executed"
-    const val TEST_2_EXECUTED = "test 2 executed"
-    const val CONTEXT_1_EXECUTED = "context 1 executed"
-    const val CONTEXT_2_EXECUTED = "context 2"
-    const val TEST_3_EXECUTED = "test 3 executed"
-    const val TEST_4_EXECUTED = "test 4 executed"
     val context = describe("test dependencies") {
-            it("are recreated for each test") {
-                // tests run in parallel, so the total order of events is not defined.
-                // we track events in a list of lists and record the events that lead to each test
-                val totalEvents = mutableListOf<List<String>>()
-                Suite {
-                    val testEvents = mutableListOf<String>()
-                    totalEvents.add(testEvents)
-                    testEvents.add(ROOT_CONTEXT_EXECUTED)
-                    autoClose("dependency", closeFunction = { testEvents.add(DEPENDENCY_CLOSED) })
-                    test("test 1") { testEvents.add(TEST_1_EXECUTED) }
-                    test("test 2") { testEvents.add(TEST_2_EXECUTED) }
-                    context("context 1") {
-                        testEvents.add(CONTEXT_1_EXECUTED)
+        it("are recreated for each test") {
+            // tests run in parallel, so the total order of events is not defined.
+            // we track events in a list of lists and record the events that lead to each test
+            val totalEvents = mutableListOf<List<Event>>()
+            Suite {
+                val testEvents = mutableListOf<Event>()
+                totalEvents.add(testEvents)
+                testEvents.add(ROOT_CONTEXT_EXECUTED)
+                autoClose("dependency", closeFunction = { testEvents.add(DEPENDENCY_CLOSED) })
+                test("test 1") { testEvents.add(TEST_1_EXECUTED) }
+                test("test 2") { testEvents.add(TEST_2_EXECUTED) }
+                context("context 1") {
+                    testEvents.add(CONTEXT_1_EXECUTED)
 
-                        context("context 2") {
-                            testEvents.add(CONTEXT_2_EXECUTED)
-                            test("test 3") { testEvents.add(TEST_3_EXECUTED) }
-                        }
+                    context("context 2") {
+                        testEvents.add(CONTEXT_2_EXECUTED)
+                        test("test 3") { testEvents.add(TEST_3_EXECUTED) }
                     }
-                    test("test4: tests can be defined after contexts") {
-                        testEvents.add(TEST_4_EXECUTED)
-                    }
-                }.run()
+                }
+                test("test4: tests can be defined after contexts") {
+                    testEvents.add(TEST_4_EXECUTED)
+                }
+            }.run()
 
-                expectThat(totalEvents)
-                    .containsExactlyInAnyOrder(
-                        listOf(ROOT_CONTEXT_EXECUTED, TEST_1_EXECUTED, DEPENDENCY_CLOSED),
-                        listOf(ROOT_CONTEXT_EXECUTED, TEST_2_EXECUTED, DEPENDENCY_CLOSED),
-                        listOf(
-                            ROOT_CONTEXT_EXECUTED,
-                            CONTEXT_1_EXECUTED,
-                            CONTEXT_2_EXECUTED,
-                            TEST_3_EXECUTED,
-                            DEPENDENCY_CLOSED
-                        ),
-                        listOf(ROOT_CONTEXT_EXECUTED, TEST_4_EXECUTED, DEPENDENCY_CLOSED)
-                    )
-            }
+            expectThat(totalEvents)
+                .containsExactlyInAnyOrder(
+                    listOf(ROOT_CONTEXT_EXECUTED, TEST_1_EXECUTED, DEPENDENCY_CLOSED),
+                    listOf(ROOT_CONTEXT_EXECUTED, TEST_2_EXECUTED, DEPENDENCY_CLOSED),
+                    listOf(
+                        ROOT_CONTEXT_EXECUTED,
+                        CONTEXT_1_EXECUTED,
+                        CONTEXT_2_EXECUTED,
+                        TEST_3_EXECUTED,
+                        DEPENDENCY_CLOSED
+                    ),
+                    listOf(ROOT_CONTEXT_EXECUTED, TEST_4_EXECUTED, DEPENDENCY_CLOSED)
+                )
         }
+    }
 }

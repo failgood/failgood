3df7a1b52654203e46315026ebb6efd5ded34034
Christoph Sturm <me@christophsturm.com>
2021-01-13 16:40:17 +0100

    don't try to find all test in TestFinderTest
    to avoid having to change it anytime I add a new test

--- failfast/src/test/kotlin/failfast/TestFinderTest.kt
+++ failfast/src/test/kotlin/failfast/TestFinderTest.kt
@@ -1,13 +1,6 @@
 package failfast
 
 import failfast.docs.ObjectTestContextTest
-import failfast.internal.ContextExecutorTest
-import failfast.internal.ContextTreeReporterTest
-import failfast.internal.ExceptionPrettyPrinterTest
-import failfast.internal.Junit4ReporterTest
-import failfast.internal.SingleTestExecutorTest
-import failfast.pitest.FailFastTestPluginFactoryTest
-import failfast.pitest.FailFastTestUnitFinderTest
 import strikt.api.expectThat
 import strikt.assertions.containsExactlyInAnyOrder
 
@@ -19,25 +12,10 @@ object TestFinderTest {
     val context =
         describe("test finder") {
             it("can find Test classes") {
-                expectThat(FailFast.findTestClasses(randomTestClass = TestFinderTest::class))
+                expectThat(FailFast.findTestClasses(classIncludeRegex = Regex(".*docs.*Test.class\$")))
                     .containsExactlyInAnyOrder(
                         ObjectTestContextTest::class,
-                        TestFinderTest::class.java
-                            .classLoader
-                            .loadClass("failfast.docs.TestContextOnTopLevelTest").kotlin,
-                        ContextExecutorTest::class,
-                        ContextTest::class,
-                        ExceptionPrettyPrinterTest::class,
-                        //                        MultiThreadingPerformanceTestNotAlways::class,
-                        SuiteTest::class,
-                        TestFinderTest::class,
-                        ObjectContextProviderTest::class,
-                        TestLifecycleTest::class,
-                        ContextTreeReporterTest::class,
-                        SingleTestExecutorTest::class,
-                        FailFastTestUnitFinderTest::class,
-                        FailFastTestPluginFactoryTest::class,
-                        Junit4ReporterTest::class
+                        TestFinderTest::class.java.classLoader.loadClass("failfast.docs.TestContextOnTopLevelTest").kotlin,
                     )
             }
         }

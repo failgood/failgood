0db9d37f5b2be4b52e433a51652a13e48c6216dd
Christoph Sturm <me@christophsturm.com>
2021-01-16 00:03:47 +0100

    don't filter stacktrace if it does not contain failfast

--- failfast/src/main/kotlin/failfast/internal/ExceptionPrettyPrinter.kt
+++ failfast/src/main/kotlin/failfast/internal/ExceptionPrettyPrinter.kt
@@ -1,10 +1,16 @@
 package failfast.internal
 
 class ExceptionPrettyPrinter(private val throwable: Throwable) {
-    val stackTrace =
-        throwable.stackTrace
+    val stackTrace = run {
+        val onlyElementsWithLineNumber = throwable.stackTrace
             .filter { it.lineNumber > 0 }
-            .dropLastWhile { !it.className.startsWith("failfast") }
+
+        val onlyFailfast = onlyElementsWithLineNumber.dropLastWhile { !it.className.startsWith("failfast") }
+        if (onlyFailfast.isEmpty())
+            onlyElementsWithLineNumber
+        else
+            onlyFailfast
+    }
 
     fun prettyPrint(): String {
         return "${throwable::class.qualifiedName} : ${throwable.message}\n\tat " +

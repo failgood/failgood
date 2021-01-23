adb8fd1926cdeb8e8e317285261cf9b74996fce9
Christoph Sturm <me@christophsturm.com>
2021-01-23 15:15:40 +0100

    add alltests and autotest, run build in parallel

---
+++ failfast-r2dbc/src/test/kotlin/failfast/r2dbc/AllTests.kt
@@ -0,0 +1,9 @@
+package failfast.r2dbc
+
+import failfast.FailFast
+
+
+fun main() {
+    FailFast.runAllTests()
+}
+
---
+++ failfast-r2dbc/src/test/kotlin/failfast/r2dbc/AutoTestMain.kt
@@ -0,0 +1,8 @@
+package failfast.r2dbc
+
+import failfast.FailFast.autoTest
+
+
+fun main() {
+    autoTest()
+}

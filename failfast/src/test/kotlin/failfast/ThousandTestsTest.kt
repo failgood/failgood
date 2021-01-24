b2dab34ea182687c94224db49003187324413579
Christoph Sturm <me@christophsturm.com>
2021-01-24 20:20:39 +0100

    add a test case that runs a context with 1000 tests

---
+++ failfast/src/test/kotlin/failfast/ThousandTestsTest.kt
@@ -0,0 +1,19 @@
+package failfast
+
+fun main() {
+    FailFast.runTest()
+}
+
+object ThousandTestsTest {
+    val context = describe("a test suite with 1000 tests in one context") {
+        test("runs pretty fast") {
+            Suite(describe("the context", false) {
+                repeat(1000) {
+                    test("test $it") {
+
+                    }
+                }
+            }).run()
+        }
+    }
+}

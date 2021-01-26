5ee67d9ada111cd045631ee2f260c3f6fc05fdfb
Christoph Sturm <me@christophsturm.com>
2021-01-26 14:11:44 +0100

    remove duplication

--- failfast/src/test/kotlin/failfast/internal/ContextExecutorTest.kt
+++ failfast/src/test/kotlin/failfast/internal/ContextExecutorTest.kt
@@ -24,64 +24,51 @@ object ContextExecutorTest {
                         context("context 4") { test("test 4") {} }
                     }
 
+                val contextInfo = coroutineScope {
+                    ContextExecutor(ctx, this).execute()
+                }
                 it("returns tests") {
-                    coroutineScope {
-                        val contextInfo = ContextExecutor(ctx, this).execute()
-                        expectThat(contextInfo.tests.keys)
-                            .containsExactlyInAnyOrder(
-                                TestDescriptor(rootContext, "test 1"),
-                                TestDescriptor(rootContext, "test 2"),
-                                TestDescriptor(rootContext, "failed test"),
-                                TestDescriptor(context2, "test 3"),
-                                TestDescriptor(context4, "test 4")
-                            )
-                    }
+                    expectThat(contextInfo.tests.keys)
+                        .containsExactlyInAnyOrder(
+                            TestDescriptor(rootContext, "test 1"),
+                            TestDescriptor(rootContext, "test 2"),
+                            TestDescriptor(rootContext, "failed test"),
+                            TestDescriptor(context2, "test 3"),
+                            TestDescriptor(context4, "test 4")
+                        )
                 }
                 it("returns deferred test results") {
-                    coroutineScope {
-                        val contextInfo = ContextExecutor(ctx, this).execute()
-                        val testResults = contextInfo.tests.values.awaitAll()
-                        val successful = testResults.filterIsInstance<Success>()
-                        val failed = testResults - successful
-                        expectThat(successful.map { it.test })
-                            .containsExactlyInAnyOrder(
-                                TestDescriptor(rootContext, "test 1"),
-                                TestDescriptor(rootContext, "test 2"),
-                                TestDescriptor(context2, "test 3"),
-                                TestDescriptor(context4, "test 4")
-                            )
-                        expectThat(failed).map { it.test }.containsExactly(TestDescriptor(rootContext, "failed test"))
-                    }
+                    val testResults = contextInfo.tests.values.awaitAll()
+                    val successful = testResults.filterIsInstance<Success>()
+                    val failed = testResults - successful
+                    expectThat(successful.map { it.test })
+                        .containsExactlyInAnyOrder(
+                            TestDescriptor(rootContext, "test 1"),
+                            TestDescriptor(rootContext, "test 2"),
+                            TestDescriptor(context2, "test 3"),
+                            TestDescriptor(context4, "test 4")
+                        )
+                    expectThat(failed).map { it.test }.containsExactly(TestDescriptor(rootContext, "failed test"))
                 }
 
                 it("returns contexts") {
-                    coroutineScope {
-                        val contextInfo = ContextExecutor(ctx, this).execute()
-                        expectThat(contextInfo.contexts)
-                            .containsExactlyInAnyOrder(rootContext, context1, context2, context4)
-                    }
+                    expectThat(contextInfo.contexts)
+                        .containsExactlyInAnyOrder(rootContext, context1, context2, context4)
                 }
                 it("reports time of successful tests") {
-                    coroutineScope {
-                        val results = ContextExecutor(ctx, this).execute()
-
-                        expectThat(results.tests.values.awaitAll().filterIsInstance<Success>())
-                            .all { get { timeMicro }.isGreaterThan(1) }
-                    }
+                    expectThat(contextInfo.tests.values.awaitAll().filterIsInstance<Success>())
+                        .all { get { timeMicro }.isGreaterThan(1) }
                 }
                 it("reports file name and line number for failed tests") {
-                    coroutineScope {
-                        val results = ContextExecutor(ctx, this).execute()
-
-                        expectThat(results.tests.values.awaitAll().filterIsInstance<Failed>()).single().and {
-                            get { stackTraceElement }.endsWith("ContextExecutorTest.kt:22)")
-                        }
+                    expectThat(contextInfo.tests.values.awaitAll().filterIsInstance<Failed>()).single().and {
+                        get { stackTraceElement }.endsWith("ContextExecutorTest.kt:22)")
                     }
 
                 }
                 describe("supports lazy execution") { itWill("find tests without executing them") {} }
             }
-            describe("handles failing contexts") {
+            describe("handles failing contexts")
+            {
                 val ctx =
                     RootContext("root context") {
                         test("test 1") {}
@@ -96,13 +83,14 @@ object ContextExecutorTest {
 
                         expectThat(results.tests.values.awaitAll().filterIsInstance<Failed>()).single().and {
                             get { test }.isEqualTo(TestDescriptor(rootContext, "context 1"))
-                            get { stackTraceElement }.endsWith("ContextExecutorTest.kt:89)")
+                            get { stackTraceElement }.endsWith("ContextExecutorTest.kt:76)")
                         }
                     }
 
                 }
             }
-            describe("detects duplicated tests") {
+            describe("detects duplicated tests")
+            {
                 it("fails with duplicate tests in one context") {
                     val ctx =
                         RootContext {
@@ -122,7 +110,8 @@ object ContextExecutorTest {
                     coroutineScope { ContextExecutor(ctx, this).execute() }
                 }
             }
-            it("can close resources") {
+            it("can close resources")
+            {
                 val events = mutableListOf<String>()
                 var closeCalled = false
                 val closable = AutoCloseable { closeCalled = true }

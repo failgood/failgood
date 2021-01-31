5ec61ecea8d4b80d5991336f66ae4809ab726aaa
Christoph Sturm <me@christophsturm.com>
2021-01-31 23:01:50 +0100

    move coroutines test, add a method that proves that i have no idea how to do multi threading with flows

---
+++ failfast/src/test/kotlin/failfast/experiments/CoroutinesTest.kt
@@ -0,0 +1,51 @@
+package failfast.experiments
+
+import failfast.uptime
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toCollection
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.Executors
+
+fun main() {
+    asyncTest()
+//    flowTest()
+}
+
+private fun asyncTest() {
+    val threadPool = Executors.newWorkStealingPool(1000)
+    threadPool.asCoroutineDispatcher()
+        .use { dispatcher ->
+            runBlocking(dispatcher) {
+                (0 until 1000)
+                    .map {
+                        async {
+                            @Suppress("BlockingMethodInNonBlockingContext")
+                            Thread.sleep(1000)
+                        }
+                    }
+                    .awaitAll()
+                println(uptime() + " " + threadPool)
+            }
+        }
+}
+
+// no idea how to do multithreading with flows
+private fun flowTest() {
+    val threadPool = Executors.newWorkStealingPool(1000)
+    threadPool.asCoroutineDispatcher()
+        .use { dispatcher ->
+            runBlocking(dispatcher) {
+                (0 until 1000).asFlow().flowOn(dispatcher).map {
+                    Thread.sleep(1000)
+                    it
+                }.toCollection(mutableListOf())
+            }
+            println(uptime() + " " + threadPool)
+        }
+}
+

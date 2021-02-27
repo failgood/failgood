c657060edeaa1067685f9adad5426391bf13a85f
Christoph Sturm <me@christophsturm.com>
2021-02-27 14:44:16 +0100

    introduce ConnectionProvider

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,4 +7,8 @@ repositories {
     gradlePluginPortal() // so that external plugins can be resolved in dependencies section
     mavenCentral()
 }
+dependencies {
+    // hotfix to make kotlin scratch files work in idea
+    implementation(kotlin("script-runtime"))
+}
 

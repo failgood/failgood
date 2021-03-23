30397bb9318dd8739df05de4113a31570245060f
Christoph Sturm <me@christophsturm.com>
2021-03-23 14:30:01 +0100

    add a Logging EngineExecutionListener

---
+++ failfast/src/main/kotlin/failfast/junit/LoggingEngineExecutionListener.kt
@@ -0,0 +1,28 @@
+package failfast.junit
+
+import org.junit.platform.engine.EngineExecutionListener
+import org.junit.platform.engine.TestDescriptor
+import org.junit.platform.engine.TestExecutionResult
+
+class LoggingEngineExecutionListener(private val delegate: EngineExecutionListener) : EngineExecutionListener {
+    val events = mutableListOf<String>()
+    fun event(s: String) {
+        events.add(s)
+    }
+
+    override fun executionSkipped(testDescriptor: TestDescriptor?, reason: String?) {
+        event("executionSkipped " + testDescriptor!!.displayName)
+        delegate.executionSkipped(testDescriptor, reason)
+    }
+
+    override fun executionStarted(testDescriptor: TestDescriptor?) {
+        event("executionStarted " + testDescriptor!!.displayName)
+        delegate.executionStarted(testDescriptor)
+    }
+
+    override fun executionFinished(testDescriptor: TestDescriptor?, testExecutionResult: TestExecutionResult?) {
+        event("executionFinished " + testDescriptor!!.displayName + ": " + testExecutionResult)
+        delegate.executionFinished(testDescriptor, testExecutionResult)
+    }
+
+}

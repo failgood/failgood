591d9dda6a23b00bf55d8d6aae62abcadb583d50
Christoph Sturm <me@christophsturm.com>
2021-03-26 18:45:01 +0100

    start a collection of problematic tests that should still work

---
+++ failfast/src/test/kotlin/failfast/problematic/EmptyRootContextTest.kt
@@ -0,0 +1,11 @@
+package failfast.problematic
+
+import failfast.describe
+
+// this is the place for test suites that are somehow problematic but should be handled gracefully
+
+object EmptyRootContextTest {
+    val context = describe("empty root context") {
+
+    }
+}

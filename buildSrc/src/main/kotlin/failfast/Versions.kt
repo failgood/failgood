a6e31ca49c49462fd6559db10165c5c381c3ab8d
Christoph Sturm <me@christophsturm.com>
2021-04-10 18:57:37 +0200

    bump strikt version

--- buildSrc/src/main/kotlin/failfast/Versions.kt
+++ buildSrc/src/main/kotlin/failfast/Versions.kt
@@ -2,7 +2,7 @@ package failfast
 
 object Versions {
     const val coroutinesVersion = "1.4.3"
-    const val striktVersion = "0.29.0"
+    const val striktVersion = "0.30.0"
     const val junitPlatformVersion = "1.7.1"
     const val pitestVersion = "1.6.4"
     const val kotlinVersion = "1.4.32"

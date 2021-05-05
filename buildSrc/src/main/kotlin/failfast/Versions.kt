59cb58c64a5bb8bc389707b46573263497fa1f26
Christoph Sturm <me@christophsturm.com>
2021-05-05 12:52:49 +0200

    bump pitest

--- buildSrc/src/main/kotlin/failfast/Versions.kt
+++ buildSrc/src/main/kotlin/failfast/Versions.kt
@@ -4,6 +4,6 @@ object Versions {
     const val coroutinesVersion = "1.4.3"
     const val striktVersion = "0.31.0"
     const val junitPlatformVersion = "1.7.1"
-    const val pitestVersion = "1.6.5"
+    const val pitestVersion = "1.6.6"
     const val kotlinVersion = "1.5.0"
 }

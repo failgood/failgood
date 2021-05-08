793738f99195ad4332294f39d624419659fc0aad
Christoph Sturm <me@christophsturm.com>
2021-05-08 17:48:56 +0200

    test multiple contexts in one object

--- failfast/src/test/kotlin/failfast/ObjectContextProviderTest.kt
+++ failfast/src/test/kotlin/failfast/ObjectContextProviderTest.kt
@@ -1,9 +1,8 @@
 package failfast
 
+import failfast.docs.ObjectMultipleContextsTest
 import strikt.api.expectThat
-import strikt.assertions.isA
-import strikt.assertions.isEqualTo
-import strikt.assertions.single
+import strikt.assertions.*
 
 fun main() {
     FailFast.runTest()
@@ -21,6 +20,11 @@ object ObjectContextProviderTest {
                 expectThat(ObjectContextProvider(TestFinderTest::class).getContexts()).single()
                     .isA<RootContext>()
                     .and { get(RootContext::name).isEqualTo("test finder") }
+            }
+            it("provides a list of contexts from an object in a kotlin class (MyTest::class)") {
+                expectThat(ObjectContextProvider(ObjectMultipleContextsTest::class).getContexts()).hasSize(2).all {
+                    isA<RootContext>()
+                }
             }
             it("provides a top level context from a kotlin class") {
                 expectThat(ObjectContextProvider(ObjectContextProviderTest::class.java.classLoader.loadClass("failfast.docs.TestContextOnTopLevelTest").kotlin).getContexts()).single()

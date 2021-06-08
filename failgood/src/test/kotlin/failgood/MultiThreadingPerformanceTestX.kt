05f12d5eff757840e2095448e7a7e8a4915beb94
Christoph Sturm <me@christophsturm.com>
2021-06-08 14:49:38 +0200

    fix test naming

--- failgood/src/test/kotlin/failgood/MultiThreadingPerformanceTestX.kt
+++ failgood/src/test/kotlin/failgood/MultiThreadingPerformanceTestX.kt
@@ -11,7 +11,7 @@ class MultiThreadingPerformanceTestX {
     val context =
         describe("multi threaded test running") {
             repeat(1000) {
-                test("sleeping test $it") {
+                test("sleeping test ${it + 1}") {
                     @Suppress("BlockingMethodInNonBlockingContext")
                     Thread.sleep(1000)
                 }

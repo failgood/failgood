5832050d6322ffcf3f9755263087e30dc6d2ccf8
Christoph Sturm <me@christophsturm.com>
2021-08-25 20:05:25 +0200

    fix formatting

--- buildSrc/src/main/kotlin/failgood/versions/Versions.kt
+++ buildSrc/src/main/kotlin/failgood/versions/Versions.kt
@@ -1,7 +1,7 @@
 package failgood.versions
 
-    const val coroutinesVersion = "1.5.1"
-    const val striktVersion = "0.31.0"
+const val coroutinesVersion = "1.5.1"
+const val striktVersion = "0.31.0"
 const val junitPlatformVersion = "1.7.2"
 const val pitestVersion = "1.6.9"
 const val kotlinVersion = "1.5.21"

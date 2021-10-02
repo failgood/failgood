e74268f7218ef8e8528e8f9021916f70a6faa135
Christoph Sturm <me@christophsturm.com>
2021-10-02 17:39:36 +0200

    add a failing test for detection of line number and class

--- failgood/src/test/kotlin/failgood/RootContextTest.kt
+++ failgood/src/test/kotlin/failgood/RootContextTest.kt
@@ -5,14 +5,29 @@ import strikt.assertions.isEqualTo
 
 @Test
 class RootContextTest {
+    // declare those on top level to make it more realistic
+    val ste1 = Throwable().stackTrace.first()
+    val testContext = RootContext {}
+    val ste2 = Throwable().stackTrace.first()
+    val testContextCreatedByMethod = createRootContext()
     val context: RootContext = describe(RootContext::class) {
-        val ste = Throwable().stackTrace.first()
-        val testContext = RootContext {}
         it("knows its file and line number") {
             expectThat(testContext.stackTraceElement) {
-                get { lineNumber }.isEqualTo(ste.lineNumber + 1)
-                get { className }.isEqualTo(ste.className)
+                get { lineNumber }.isEqualTo(ste1.lineNumber + 1)
+                get { className }.isEqualTo(ste1.className)
+            }
+        }
+        it("knows file and line number even when created by a utility method") {
+            expectThat(testContextCreatedByMethod.stackTraceElement) {
+                get { lineNumber }.isEqualTo(ste2.lineNumber + 1)
+                get { className }.isEqualTo(ste2.className)
             }
         }
     }
+
+}
+
+private fun createRootContext(): RootContext {
+    Throwable().printStackTrace()
+    return RootContext {}
 }

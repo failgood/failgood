e2e1f7b0ea7981ef164554f2f18b789321624304
Christoph Sturm <me@christophsturm.com>
2021-10-15 18:18:13 +0200

    bump pitest

--- buildSrc/src/main/kotlin/failgood/versions/Versions.kt
+++ buildSrc/src/main/kotlin/failgood/versions/Versions.kt
@@ -3,5 +3,5 @@ package failgood.versions
 const val coroutinesVersion = "1.5.1"
 const val striktVersion = "0.32.0"
 const val junitPlatformVersion = "1.8.1"
-const val pitestVersion = "1.7.1"
+const val pitestVersion = "1.7.2"
 const val kotlinVersion = "1.5.31"

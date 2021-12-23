778904110bb237a4167171508c0d2b99b5f9d077
Christoph Sturm <me@christophsturm.com>
2021-12-23 15:39:01 +0100

    start testing ResourcesCloser

---
+++ failgood/src/test/kotlin/failgood/internal/ResourcesCloserTest.kt
@@ -0,0 +1,23 @@
+package failgood.internal
+
+import failgood.Test
+import failgood.describe
+import failgood.mock.call
+import failgood.mock.getCalls
+import failgood.mock.mock
+import kotlinx.coroutines.coroutineScope
+import strikt.api.expectThat
+import strikt.assertions.containsExactly
+
+@Test
+class ResourcesCloserTest {
+    val context = describe(ResourcesCloser::class) {
+        val subject = coroutineScope { ResourcesCloser(this) }
+        it("closes autoclosables") {
+            val autoCloseable = mock<AutoCloseable>()
+            subject.autoClose(autoCloseable)
+            subject.close()
+            expectThat(getCalls(autoCloseable)).containsExactly(call(AutoCloseable::close))
+        }
+    }
+}

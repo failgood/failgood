cb1a318dd0ad1404d54529bdac3b3de7c77dc36a
Christoph Sturm <me@christophsturm.com>
2021-12-23 15:16:56 +0100

    bump coroutines

--- buildSrc/src/main/kotlin/failgood/versions/Versions.kt
+++ buildSrc/src/main/kotlin/failgood/versions/Versions.kt
@@ -1,6 +1,6 @@
 package failgood.versions
 
-const val coroutinesVersion = "1.5.2"
+const val coroutinesVersion = "1.6.0"
 const val striktVersion = "0.33.0"
 const val junitPlatformVersion = "1.8.2"
 const val pitestVersion = "1.7.3"

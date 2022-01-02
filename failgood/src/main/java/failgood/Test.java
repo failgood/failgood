49d4a712baf829dc9623bdeb91ebc9c78fc29662
Christoph Sturm <me@christophsturm.com>
2022-01-02 11:46:44 +0100

    add javadoc to fix warning

--- failgood/src/main/java/failgood/Test.java
+++ failgood/src/main/java/failgood/Test.java
@@ -2,6 +2,9 @@ package failgood;
 
 import org.junit.platform.commons.annotation.Testable;
 
+/**
+ * This annotation tells the junit platform that a class or object is a failgood test.
+ */
 @Testable
 public @interface Test {
 }

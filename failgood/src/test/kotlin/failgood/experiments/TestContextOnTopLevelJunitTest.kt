f4543eb9ce6a3a82f5273bcd650152aa8edd0f00
Christoph Sturm <me@christophsturm.com>
2022-01-02 12:07:10 +0100

    prototype another way of declaring test contexts

---
+++ failgood/src/test/kotlin/failgood/experiments/TestContextOnTopLevelJunitTest.kt
@@ -0,0 +1,24 @@
+package failgood.experiments
+
+import failgood.ContextLambda
+import failgood.RootContext
+import failgood.Test
+
+/*
+this is not yet implemented, but could be cool
+ */
+class TestContextOnTopLevelJunitTest {
+    @Test
+    fun `top level contexts can be declared as function`() =
+        describe {
+            it("makes it possible to have multiple top level contexts in a class and run them separately from idea") {}
+        }
+}
+
+fun describe(
+    disabled: Boolean = false,
+    order: Int = 0,
+    isolation: Boolean = true,
+    function: ContextLambda
+):
+        RootContext = RootContext("todo", disabled, order, isolation, function = function)

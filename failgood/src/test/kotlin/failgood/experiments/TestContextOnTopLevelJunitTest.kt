2f09ec6d7ba64ca591d3a008fd8ed2c031e1fe53
Christoph Sturm <me@christophsturm.com>
2022-02-07 21:36:00 +0100

    update comment

--- failgood/src/test/kotlin/failgood/experiments/TestContextOnTopLevelJunitTest.kt
+++ failgood/src/test/kotlin/failgood/experiments/TestContextOnTopLevelJunitTest.kt
@@ -5,7 +5,7 @@ import failgood.RootContext
 import failgood.Test
 
 /*
-this is not yet implemented, but could be cool
+this already works, but its not yet possible to run via the icon on the method. it is possible to run via the class
  */
 class TestContextOnTopLevelJunitTest {
     @Test

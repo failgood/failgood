3dad9191a70d092e12abdee4c5def0d9f7df085a
Christoph Sturm <me@christophsturm.com>
2022-02-12 20:56:39 +0100

    invite people to the slack channel

--- failgood/src/main/kotlin/failgood/junit/FailGoodJunitTestEngine.kt
+++ failgood/src/main/kotlin/failgood/junit/FailGoodJunitTestEngine.kt
@@ -192,7 +192,9 @@ class FailGoodJunitTestEngine : TestEngine {
         } catch (e: Exception) {
             println(
                 "exception occurred inside failgood.\n" +
-                    "if you run the latest version please submit a bug at https://github.com/failgood/failgood/issues"
+                        "if you run the latest version please submit a bug at " +
+                        "https://github.com/failgood/failgood/issues " +
+                        "or tell someone in the #failgood channel in the kotlin-lang slack"
             )
             e.printStackTrace()
             throw e

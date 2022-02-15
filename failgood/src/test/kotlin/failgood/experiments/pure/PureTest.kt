3cd2cec8d1003a4c97f54398077332f2100d737d
Christoph Sturm <me@christophsturm.com>
2022-02-15 21:23:55 +0100

    add a vararg version

--- failgood/src/test/kotlin/failgood/experiments/pure/PureTest.kt
+++ failgood/src/test/kotlin/failgood/experiments/pure/PureTest.kt
@@ -21,8 +21,37 @@ class PureTest {
 
     class MongoDB
 
-    private fun <Fixture> context(name: String, fixture: suspend () -> Fixture, listOf: List<Node<Fixture>>) =
-        Context(name, fixture, listOf)
+    private fun <Fixture> context(name: String, fixture: suspend () -> Fixture, children: List<Node<Fixture>>) =
+        Context(name, fixture, children)
+
+    private fun <Fixture> test(name: String, function: suspend (Fixture) -> Unit) = Test(name, function)
+
+    sealed interface Node<out Fixture>
+    private data class Test<Fixture>(val name: String, val function: suspend (Fixture) -> Unit) : Node<Fixture>
+    private data class Context<Fixture>(
+        val name: String,
+        val fixture: suspend () -> Fixture,
+        val children: List<Node<Fixture>>
+    ) : Node<Nothing>
+}
+
+/*
+ * the same but with varargs
+ */
+class PureTestVarargs {
+    private val suite = context(
+        "root", fixture = { MongoDB() },
+        test("a test") { mongoDb: MongoDB -> },
+        context(
+            "a subcontext", {},
+            test("another test") {},
+        )
+    )
+
+    class MongoDB
+
+    private fun <Fixture> context(name: String, fixture: suspend () -> Fixture, vararg children: Node<Fixture>) =
+        Context(name, fixture, children.asList())
 
     private fun <Fixture> test(name: String, function: suspend (Fixture) -> Unit) = Test(name, function)
 

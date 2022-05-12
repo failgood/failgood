c2d433cf320757d895e32c5feaed4f90be3d1a7b
Christoph Sturm <me@christophsturm.com>
2022-05-12 21:24:37 +0200

    return result

--- failgood/src/test/kotlin/failgood/tools/RetryForTests.kt
+++ failgood/src/test/kotlin/failgood/tools/RetryForTests.kt
@@ -1,4 +1,3 @@
-
 package failgood.tools
 
 import failgood.Test
@@ -11,11 +10,12 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.ExperimentalTime
+
 @Test
 class RetryForTests {
     @OptIn(ExperimentalTime::class)
-    val tests = describe(::retryFor.name) {
-        it("retries when time is not yet up") {
+    val tests = describe("retryFor") {
+        it("rethrows exception when time is up") {
             val exception = assertNotNull(kotlin.runCatching {
                 retryFor(1.milliseconds) {
                     throw RuntimeException("error!")
@@ -23,12 +23,20 @@ class RetryForTests {
             }.exceptionOrNull())
             assert(exception.message == "error!")
         }
-
+        it("returns result") {
+            var calls = 1
+            val result = retryFor(100.milliseconds) {
+                if (calls-- > 0)
+                    throw RuntimeException("error!")
+                "result"
+            }
+            assert(result == "result")
+        }
     }
 }
 
 @OptIn(ExperimentalTime::class)
-private suspend fun retryFor(duration: Duration = 1.seconds, function: suspend () -> Unit) {
+private suspend fun <Result> retryFor(duration: Duration = 1.seconds, function: suspend () -> Result): Result{
     val until = Instant.now().plus(duration.inWholeMilliseconds, ChronoUnit.MILLIS)
     while (true) {
         try {
@@ -36,7 +44,7 @@ private suspend fun retryFor(duration: Duration = 1.seconds, function: suspend (
         } catch (e: Throwable) {
             if (Instant.now().isAfter(until))
                 throw e
-            delay(100)
+            delay(10)
         }
     }
 

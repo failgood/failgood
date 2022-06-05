ac5fd5cdce6e040c65a1f7073ac39ad51039f837
Christoph Sturm <me@christophsturm.com>
2022-06-05 21:09:14 +0200

    give the failgood descriptor a class source
    mostly for fun but it also stops gradle from logging "unknow class" in debug mode

--- failgood/src/main/kotlin/failgood/junit/FailGoodEngineDescriptor.kt
+++ failgood/src/main/kotlin/failgood/junit/FailGoodEngineDescriptor.kt
@@ -3,8 +3,11 @@ package failgood.junit
 import failgood.internal.ContextResult
 import failgood.internal.FailedRootContext
 import failgood.internal.SuiteExecutionContext
+import org.junit.platform.engine.TestSource
 import org.junit.platform.engine.UniqueId
+import org.junit.platform.engine.support.descriptor.ClassSource
 import org.junit.platform.engine.support.descriptor.EngineDescriptor
+import java.util.*
 
 internal class FailGoodEngineDescriptor(
     uniqueId: UniqueId,
@@ -15,6 +18,12 @@ internal class FailGoodEngineDescriptor(
     val failedRootContexts = mutableListOf<FailedRootContext>()
     val mapper = TestMapper()
     fun allDescendants(): String {
-        return descendants.joinToString(postfix = "(total:${descendants.size})") { "[UUID:${it.uniqueId} TYPE:${it.type}]" }
+        val size = descendants.size
+        if (size == 0)
+            return "<EMPTY>"
+        return descendants.joinToString(postfix = "(total:$size)") { "[UUID:${it.uniqueId} TYPE:${it.type}]" }
     }
+
+    private val failgoodClass: Optional<TestSource> = Optional.of(ClassSource.from(FailGoodJunitTestEngine::class.java))
+    override fun getSource(): Optional<TestSource> = failgoodClass
 }

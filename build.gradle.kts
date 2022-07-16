864547c69d4fadc96820bdb5c0ede055ac1efa69
Christoph Sturm <me@christophsturm.com>
2022-07-16 14:31:23 +0200

    remove obsolete comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ tasks.wrapper { distributionType = Wrapper.DistributionType.ALL }
 
 nexusPublishing {
     repositories {
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+        sonatype {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         }

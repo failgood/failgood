546200dd2fd00b605e8953e4f452d72259cf5de1
Christoph Sturm <me@christophsturm.com>
2022-08-07 02:01:59 +0200

    add failing test for callbacks without isolation

--- failgood/jvm/test/failgood/functional/SubContextIsolationTest.kt
+++ failgood/jvm/test/failgood/functional/SubContextIsolationTest.kt
@@ -9,12 +9,11 @@ import java.util.concurrent.CopyOnWriteArrayList
 object SubContextIsolationTest {
     val tests = describe("sub context isolation") {
         describe("on a root context with default isolation (=ON)") {
-            val globalEvents = CopyOnWriteArrayList<CopyOnWriteArrayList<String>>()
+            val evt = NestedEvents()
             it("can turn isolation off for subcontexts") {
                 Suite(
                     describe("root", isolation = true, disabled = false) {
-                        val events = CopyOnWriteArrayList<String>()
-                        globalEvents.add(events)
+                        val events = evt.addEvent()
                         withoutIsolation {
                             describe("child") {
                                 events.add("childContext")
@@ -24,13 +23,12 @@ object SubContextIsolationTest {
                         }
                     }
                 ).run(silent = true)
-                assert(globalEvents.single().sorted() == listOf("childContext", "test1", "test2").sorted())
+                assert(evt.globalEvents.single().sorted() == listOf("childContext", "test1", "test2").sorted())
             }
             it("does not affect other contexts") {
                 Suite(
                     describe("root", isolation = true, disabled = false) {
-                        val events = CopyOnWriteArrayList<String>()
-                        globalEvents.add(events)
+                        val events = evt.addEvent()
                         withoutIsolation {
                             describe("child") {
                                 events.add("childContext")
@@ -45,11 +43,49 @@ object SubContextIsolationTest {
                         }
                     }
                 ).run(silent = true)
-                assert(globalEvents.size == 3)
-                assert(globalEvents[0].sorted() == listOf("childContext", "test1", "test2").sorted())
-                assert(globalEvents[1] == listOf("child with isolation", "test3"))
-                assert(globalEvents[2] == listOf("child with isolation", "test4"))
+
+                val e = evt.globalEvents
+                assert(e.size == 3)
+                assert(e[0].sorted() == listOf("childContext", "test1", "test2").sorted())
+                assert(e[1] == listOf("child with isolation", "test3"))
+                assert(e[2] == listOf("child with isolation", "test4"))
+            }
+            it("calls callbacks at the correct time") {
+                Suite(
+                    describe("root", isolation = true, disabled = false) {
+                        val events = evt.addEvent()
+                        withoutIsolation {
+                            describe("child") {
+                                afterEach { events.add("no-isolation-afterEach") }
+                                autoClose("yo") { events.add("no-isolation-autoClose") }
+                                events.add("childContext")
+                                it("test1") { events.add("test1") }
+                                it("test2") { events.add("test2") }
+                            }
+                        }
+                        describe("child with isolation") {
+                            events.add("child with isolation")
+                            it("test3") { events.add("test3") }
+                            it("test4") { events.add("test4") }
+                        }
+                    }
+                ).run(silent = true)
+
+                val e = evt.globalEvents
+                assert(e.size == 3)
+                val noIsolationRun = e[0]
+                assert(noIsolationRun.take(3).sorted() == listOf("childContext", "test1", "test2").sorted())
+                assert(
+                    noIsolationRun.takeLast(2).sorted() ==
+                        listOf("no-isolation-afterEach", "no-isolation-autoClose")
+                )
+                assert(e[1] == listOf("child with isolation", "test3"))
+                assert(e[2] == listOf("child with isolation", "test4"))
             }
         }
     }
+}
+private class NestedEvents {
+    val globalEvents = CopyOnWriteArrayList<CopyOnWriteArrayList<String>>()
+    fun addEvent() = CopyOnWriteArrayList<String>().also { globalEvents.add(it) }
 }

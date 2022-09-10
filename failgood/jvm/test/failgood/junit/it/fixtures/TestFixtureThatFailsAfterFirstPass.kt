fbec3c4329afdd3a2f5e0bfa47d683a7e6fc6da7
Christoph Sturm <me@christophsturm.com>
2022-09-10 11:58:49 +0200

    make the tests fixture simple again because the clever version did not work as intended.
    clever = bad :)

--- failgood/jvm/test/failgood/junit/it/fixtures/TestFixtureThatFailsAfterFirstPass.kt
+++ failgood/jvm/test/failgood/junit/it/fixtures/TestFixtureThatFailsAfterFirstPass.kt
@@ -6,13 +6,9 @@ import failgood.internal.TestFixture
 @TestFixture
 class TestFixtureThatFailsAfterFirstPass {
     var invocations = 0
-    val contextThatFailsAfter1 = rootContext(1)
-    val contextThatFailsAfter2 = rootContext(2)
-    val contextThatFailsAfter3 = rootContext(3) // let's test another version because its so easy
-
-    private fun rootContext(failAfter: Int) = describe("a test context that fails after $failAfter passes") {
+    val contextThatFailsAfter2 = describe("a test context that fails after 2 passes") {
         // not sure why it needs 3 invocations to trigger this bug.
-        if (invocations++ == failAfter)
+        if (invocations++ == 2)
             throw RuntimeException()
         it("test") {}
         describe("sub context") {

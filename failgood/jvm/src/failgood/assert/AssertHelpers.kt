db637990e61b5bdbaa82ab4727cd366af4d7da51
Christoph Sturm <me@christophsturm.com>
2022-10-05 23:59:25 +0200

    simplify

--- failgood/jvm/src/failgood/assert/AssertHelpers.kt
+++ failgood/jvm/src/failgood/assert/AssertHelpers.kt
@@ -7,4 +7,4 @@ fun <T> Iterable<T>.containsExactlyInAnyOrder(vararg elements: T) =
     this.containsExactlyInAnyOrder(elements.asList())
 
 fun <T> List<T>.endsWith(elements: List<T>) = this.takeLast(elements.size) == elements
-fun <T> List<T>.endsWith(vararg elements: T) = this.takeLast(elements.size).endsWith(elements.asList())
+fun <T> List<T>.endsWith(vararg elements: T) = this.endsWith(elements.asList())

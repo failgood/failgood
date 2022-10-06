260a35018c2608956e3f6062beb0c8d70d16a198
Christoph Sturm <me@christophsturm.com>
2022-10-06 11:32:24 +0200

    simplify test and make assertion errors easier to understand

--- failgood/jvm/test/failgood/assert/AssertHelpersTest.kt
+++ failgood/jvm/test/failgood/assert/AssertHelpersTest.kt
@@ -6,33 +6,47 @@ import failgood.describe
 @Test
 object AssertHelpersTest {
     val tests = describe("AssertHelpers") {
-        // use entries that do not implement comparable
-        val a = String::class
-        val b = Int::class
-        val c = Long::class
-        val list = listOf(a, b, c)
         describe("containsExactlyInAnyOrder") {
-            it("works with list") {
-                assert(list.containsExactlyInAnyOrder(listOf(a, b, c)))
-                assert(list.containsExactlyInAnyOrder(listOf(a, c, b)))
-                assert(!list.containsExactlyInAnyOrder(listOf(a, b)))
+            describe("simple") {
+                it("returns true when the elements are the same") {
+                    assert(listOf("a", "b").containsExactlyInAnyOrder("b", "a"))
+                }
+                it("returns true false the elements are not the same") {
+                    assert(!listOf("a", "b", "c").containsExactlyInAnyOrder("b", "a"))
+                }
             }
-            it("works with vararg") {
-                assert(list.containsExactlyInAnyOrder(a, b, c))
-                assert(list.containsExactlyInAnyOrder(a, c, b))
-                assert(!list.containsExactlyInAnyOrder(a, b))
+            describe("with a class that does not implement comparable") {
+                class C(val v:String) {
+                    override fun toString(): String {
+                        return "C(v='$v')"
+                    }
+                }
+                // use entries that do not implement comparable
+                val a = C("A")
+                val b = C("B")
+                val c = C("C")
+                val list = listOf(a, b, c)
+                it("works with list") {
+                    assert(list.containsExactlyInAnyOrder(listOf(a, b, c)))
+                    assert(list.containsExactlyInAnyOrder(listOf(a, c, b)))
+                    assert(!list.containsExactlyInAnyOrder(listOf(a, b)))
+                }
+                it("works with vararg") {
+                    assert(list.containsExactlyInAnyOrder(a, b, c))
+                    assert(list.containsExactlyInAnyOrder(a, c, b))
+                    assert(!list.containsExactlyInAnyOrder(a, b))
+                }
+
             }
         }
         describe("endsWith") {
             it("works with list") {
-                assert(list.endsWith(listOf(c)))
-                assert(list.endsWith(listOf(b, c)))
-                assert(!list.endsWith(listOf(c, b)))
+                assert(listOf("a","b","c").endsWith(listOf("c")))
+                assert(listOf("a","b","c").endsWith(listOf("b","c")))
+                assert(listOf("a","b","c").endsWith(listOf("a","b","c")))
             }
             it("works with vararg") {
-                assert(list.endsWith(c))
-                assert(list.endsWith(b, c))
-                assert(!list.endsWith(c, b))
+                assert(listOf("a","b","c").endsWith("b","c"))
             }
         }
     }

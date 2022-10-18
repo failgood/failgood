5aa36012ab7066d38506f8013ec1cd1c050ba911
Christoph Sturm <me@christophsturm.com>
2022-10-19 00:31:02 +0200

    add kdoc

--- failgood/jvm/src/failgood/util/RetryFor.kt
+++ failgood/jvm/src/failgood/util/RetryFor.kt
@@ -6,12 +6,15 @@ import java.time.temporal.ChronoUnit
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
-suspend fun <Result> retryFor(duration: Duration = 1.seconds, function: suspend () -> Result): Result {
+/**
+ * retries a [block] for a [duration], but at least once
+ */
+suspend fun <Result> retryFor(duration: Duration = 1.seconds, block: suspend () -> Result): Result {
     var calls = 0
     val until = Instant.now().plus(duration.inWholeMilliseconds, ChronoUnit.MILLIS)
     while (true) {
         try {
-            return function()
+            return block()
         } catch (e: Throwable) {
             if (calls++ != 0)
                 if (Instant.now().isAfter(until))

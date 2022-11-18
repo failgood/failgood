1bfaa9a3fd6b17e819763e8f3db944f25afc0efc
Christoph Sturm <me@christophsturm.com>
2022-11-18 21:08:36 +0100

    add a junit test to compare what gradle reports.
    move test project to top level to make it easier to edit in IDEA

--- simple-gradle-project/build.gradle.kts
+++ simple-gradle-project/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 
 dependencies {
     testImplementation("dev.failgood:failgood:0.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
 
 tasks {

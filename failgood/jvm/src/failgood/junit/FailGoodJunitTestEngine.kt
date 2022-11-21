e45bf77d9fc9b7f3da0c3dbae659e8aaaa8a3fa5
Christoph Sturm <me@christophsturm.com>
2022-11-22 00:27:55 +0100

    write debug log with unique filename

--- failgood/jvm/src/failgood/junit/FailGoodJunitTestEngine.kt
+++ failgood/jvm/src/failgood/junit/FailGoodJunitTestEngine.kt
@@ -195,7 +195,7 @@ class FailGoodJunitTestEngine : TestEngine {
             watchdog?.close()
         }
         if (debug)
-            File("failgood.debug.txt").writeText(failureLogger.envString())
+            File("failgood.debug-${UUID.randomUUID().toString().take(3)}.txt").writeText(failureLogger.envString())
         println("finished after ${uptime()}")
     }
 }

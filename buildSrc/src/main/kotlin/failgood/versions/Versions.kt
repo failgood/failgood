6c7c0f4fe92b239d37907688b0a03d8aa693e679
Christoph Sturm <me@christophsturm.com>
2023-03-09 23:49:21 +0100

    use coroutines 1.7.0-beta

--- buildSrc/src/main/kotlin/failgood/versions/Versions.kt
+++ buildSrc/src/main/kotlin/failgood/versions/Versions.kt
@@ -1,6 +1,6 @@
 package failgood.versions
 
-const val coroutinesVersion = "1.6.4"
+const val coroutinesVersion = "1.7.0-Beta"
 const val striktVersion = "0.34.1"
 const val junitPlatformVersion = "1.9.1"
 const val pitestVersion = "1.10.3"

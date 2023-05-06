ed38a6292f8c08b4aca059beefe49cd1d0f885e5
Christoph Sturm <me@christophsturm.com>
2023-05-06 15:19:59 +0200

    use kotlin 1.8.21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,6 @@ dependencyResolutionManagement {
 //    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS) // kover does not work with this
 }
 rootProject.name = "failgood-root"
-include("failgood", "failgood-examples", "failgood-gradle-test", "experiments:failgood-debugger")
+include("failgood", "failgood-examples", "failgood-gradle-test"
+    , "experiments:failgood-debugger"
+    )

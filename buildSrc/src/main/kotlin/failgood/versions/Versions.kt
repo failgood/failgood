0ab7ab827bc640511341452998bc3bdab9ffdb70
Christoph Sturm <me@christophsturm.com>
2023-05-06 15:01:32 +0200

    dep bump

--- buildSrc/src/main/kotlin/failgood/versions/Versions.kt
+++ buildSrc/src/main/kotlin/failgood/versions/Versions.kt
@@ -1,6 +1,6 @@
 package failgood.versions
 
-const val coroutinesVersion = "1.6.4"
+const val coroutinesVersion = "1.7.0"
 const val striktVersion = "0.34.1"
-const val junitPlatformVersion = "1.9.2"
-const val pitestVersion = "1.11.5"
+const val junitPlatformVersion = "1.9.3"
+const val pitestVersion = "1.13.1"

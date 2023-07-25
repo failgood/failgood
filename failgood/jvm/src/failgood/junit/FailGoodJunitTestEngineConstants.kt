fcac812ddfc91d98ac79efeae34888ff9bf68521
Christoph Sturm <me@christophsturm.com>
2023-07-25 15:16:56 +0200

    extract constant for NEW_JUNIT

--- failgood/jvm/src/failgood/junit/FailGoodJunitTestEngineConstants.kt
+++ failgood/jvm/src/failgood/junit/FailGoodJunitTestEngineConstants.kt
@@ -5,6 +5,8 @@ object FailGoodJunitTestEngineConstants {
     const val displayName = "FailGood"
     const val CONFIG_KEY_DEBUG = "failgood.debug"
 
-    // this config setting is only used by failgood's own test suite to trigger execution of test fixtures
+    // this config setting is only used by FailGood's own test suite to trigger execution of test fixtures
     const val RUN_TEST_FIXTURES = "failgood.internal.runTestFixtures"
+    const val FAILGOOD_NEW_JUNIT = "failgood.new.junit"
+
 }
--- failgood/jvm/src/failgood/junit/delegate/DelegatingJunitEngine.kt
+++ failgood/jvm/src/failgood/junit/delegate/DelegatingJunitEngine.kt
@@ -2,12 +2,14 @@ package failgood.junit.delegate
 
 import failgood.junit.FailGoodJunitTestEngine
 import failgood.junit.FailGoodJunitTestEngineConstants
+import failgood.junit.FailGoodJunitTestEngineConstants.FAILGOOD_NEW_JUNIT
 import failgood.junit.next.NewJunitEngine
 import org.junit.platform.engine.EngineDiscoveryRequest
 import org.junit.platform.engine.ExecutionRequest
 import org.junit.platform.engine.TestDescriptor
 import org.junit.platform.engine.TestEngine
 import org.junit.platform.engine.UniqueId
+
 
 class DelegatingJunitEngine : TestEngine {
     override fun getId(): String = FailGoodJunitTestEngineConstants.id
@@ -18,7 +20,7 @@ class DelegatingJunitEngine : TestEngine {
         uniqueId: UniqueId
     ): TestDescriptor {
         val useNew =
-            discoveryRequest.configurationParameters.getBoolean("failgood.new.junit").orElse(false)
+            discoveryRequest.configurationParameters.getBoolean(FAILGOOD_NEW_JUNIT).orElse(false)
         currentEngine = if (useNew) NewJunitEngine() else FailGoodJunitTestEngine()
         return currentEngine.discover(discoveryRequest, uniqueId)
     }

06dd4fce07b988780bad5d7b0503edf17c0ec10e
Christoph Sturm <me@christophsturm.com>
2023-08-17 12:16:43 +0200

    bump gradle

--- failgood-gradle-test/build.gradle.kts
+++ failgood-gradle-test/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 dependencies {
     testImplementation(project(":failgood"))
-    testImplementation("org.gradle:gradle-tooling-api:8.2.1")
+    testImplementation("org.gradle:gradle-tooling-api:8.3")
     testImplementation(kotlin("stdlib-jdk8"))
 }
 

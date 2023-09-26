1af15e0cb1fd0b7d5b8c1f50264188380c83f991
Christoph Sturm <me@christophsturm.com>
2023-09-26 23:32:59 +0200

    move one test to common

--- failgood/common/src/failgood/Test.kt
+++ failgood/common/src/failgood/Test.kt
@@ -1,11 +1,8 @@
 package failgood
 
-import org.junit.platform.commons.annotation.Testable
-
 /**
  * Indicate that something contains failgood tests. Works on object, class, function or files.
  */
-@Testable
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FILE, AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
-actual annotation class Test
+expect annotation class Test

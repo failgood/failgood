bba1c149dd82784c34dd75a98ed3673bb841e4a6
Christoph Sturm <me@christophsturm.com>
2023-09-29 12:20:43 +0200

    convert Ignore.Until to an example.

---
+++ failgood/jvm/src/failgood/examples/Until.kt
@@ -0,0 +1,23 @@
+package failgood.examples
+
+import failgood.Ignored
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneOffset
+
+/**
+ * Example for a custom project specific ignore:
+ * Ignore a test until a specific date. Useful when you want to make sure that you don't forget about a pending test
+ * Be careful: this will break your build on that date, so don't use this feature if that is a problem for you.
+ */
+class Until(private val dateString: String) : Ignored {
+
+    override fun isIgnored(): String? {
+        val date = LocalDate.parse(dateString).atStartOfDay().toInstant(ZoneOffset.UTC)
+        val now = Instant.now()
+        return if (date.isAfter(now))
+            "$dateString is after now"
+        else
+            null
+    }
+}

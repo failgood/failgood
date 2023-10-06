02f564c4a7b5df9182508810ff5347bb975a11e6
Christoph Sturm <me@christophsturm.com>
2023-10-06 21:55:51 +0200

    bump gradle and dokka

--- failgood-gradle-test/build.gradle.kts
+++ failgood-gradle-test/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 dependencies {
     testImplementation(project(":failgood"))
-    testImplementation("org.gradle:gradle-tooling-api:8.3")
+    testImplementation("org.gradle:gradle-tooling-api:8.4")
     testImplementation(kotlin("stdlib-jdk8"))
 }
 

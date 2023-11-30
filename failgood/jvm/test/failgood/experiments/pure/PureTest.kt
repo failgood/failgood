9a336505093e96ce8d66571b4fce8e3e06ca578c
Christoph Sturm <me@christophsturm.com>
2023-11-30 19:31:49 +0100

    combine list and vararg based pure test example

--- failgood/jvm/test/failgood/experiments/pure/PureTest.kt
+++ failgood/jvm/test/failgood/experiments/pure/PureTest.kt
@@ -4,7 +4,7 @@ package failgood.experiments.pure
 
 /** possible syntax for a suite without side effects */
 class PureTest {
-    private val suite =
+    private val withListOfNodes =
         context(
             "root",
             given = { MongoDB() },
@@ -21,6 +21,24 @@ class PureTest {
                         }
                     }
                 )
+            )
+        )
+    private val withVarargNodes =
+        context(
+            "root",
+            given = { MongoDB() },
+            test("a test") { mongoDb: MongoDB -> },
+            context("a subcontext", {}, test("another test") {}),
+            context(
+                "dynamic tests",
+                { UpperCaser() },
+                *(listOf(Pair("chris", "CHRIS"), Pair("freddy", "FREDDY"))
+                    .map { (name, uppercaseName) ->
+                        test("uppercases $name to $uppercaseName") { uppercaser: UpperCaser ->
+                            assert(uppercaser.toUpperCase(name) == uppercaseName)
+                        }
+                    }
+                    .toTypedArray())
             )
         )
 
@@ -51,57 +69,10 @@ class PureTest {
         val given: suspend () -> GivenType,
         val children: List<Node<GivenType>>
     ) : Node<Nothing>
-}
-
-/*
- * the same but with varargs
- */
-class PureTestVarargs {
-    private val suite =
-        context(
-            "root",
-            given = { MongoDB() },
-            test("a test") { mongoDb: MongoDB -> },
-            context("a subcontext", {}, test("another test") {}),
-            context(
-                "dynamic tests",
-                { PureTest.UpperCaser() },
-                *(listOf(Pair("chris", "CHRIS"), Pair("freddy", "FREDDY"))
-                    .map { (name, uppercaseName) ->
-                        test("uppercases $name to $uppercaseName") { uppercaser: PureTest.UpperCaser
-                            ->
-                            assert(uppercaser.toUpperCase(name) == uppercaseName)
-                        }
-                    }
-                    .toTypedArray())
-            )
-        )
-
-    class MongoDB
-
-    class UpperCaser {
-        @Suppress("SameReturnValue") fun toUpperCase(name: String): String = "not yet"
-    }
 
     private fun <GivenType> context(
         name: String,
         given: suspend () -> GivenType,
         vararg children: Node<GivenType>
     ) = Context(name, given, children.asList())
-
-    private fun <GivenType> test(name: String, function: suspend (GivenType) -> Unit) =
-        Test(name, function)
-
-    sealed interface Node<out GivenType>
-
-    private data class Test<GivenType>(
-        val name: String,
-        val function: suspend (GivenType) -> Unit
-    ) : Node<GivenType>
-
-    private data class Context<GivenType>(
-        val name: String,
-        val given: suspend () -> GivenType,
-        val children: List<Node<GivenType>>
-    ) : Node<Nothing>
 }

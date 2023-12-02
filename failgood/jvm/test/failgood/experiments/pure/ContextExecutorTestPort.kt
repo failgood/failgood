94cb6f26e14a018574da7a34768f4c7955bf0d28
Christoph Sturm <me@christophsturm.com>
2023-12-02 15:22:31 +0100

    make the given nicer by using a class

--- failgood/jvm/test/failgood/experiments/pure/ContextExecutorTestPort.kt
+++ failgood/jvm/test/failgood/experiments/pure/ContextExecutorTestPort.kt
@@ -4,35 +4,32 @@ import failgood.Ignored
 import failgood.RootContext
 import kotlinx.coroutines.delay
 
+/**
+ * here I am trying to port one of the most complex tests to the pure given dsl. nothing to see here
+ * for now.
+ */
 object ContextExecutorTestPort {
-    data class Given(
-        val assertionError: java.lang.AssertionError? = null,
-        var context: RootContext? = null
-    )
+    class Given {
+        val assertionError: java.lang.AssertionError = AssertionError("failed")
+        val context: RootContext =
+            RootContext("root context") {
+                test("test 1") { delay(1) }
+                test("test 2") { delay(1) }
+                test("ignored test", ignored = Ignored.Because("testing")) {}
+                test("failed test") { throw assertionError }
+                context("context 1") {
+                    // comment to make sure that context1 and context2 are not on the same
+                    // line
+                    context("context 2") { test("test 3") { delay(1) } }
+                }
+                context("context 3") { test("test 4") { delay(1) } }
+            }
+    }
 
     val test =
         context(
             "contextExecutorTest port",
-            given = {
-                val given = Given()
-                given.context =
-                    RootContext("root context") {
-                        test("test 1") { delay(1) }
-                        test("test 2") { delay(1) }
-                        test("ignored test", ignored = Ignored.Because("testing")) {}
-                        test("failed test") {
-                            given.assertionError = AssertionError("failed")
-                            throw given.assertionError!!
-                        }
-                        context("context 1") {
-                            // comment to make sure that context1 and context2 are not on the same
-                            // line
-                            context("context 2") { test("test 3") { delay(1) } }
-                        }
-                        context("context 3") { test("test 4") { delay(1) } }
-                    }
-                given
-            },
+            given = { Given() },
             context("executing all the tests", given = {})
         )
 }

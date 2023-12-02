5259d2cb73644c4196d2095dfd70b2126ebcacc3
Christoph Sturm <me@christophsturm.com>
2023-12-02 17:04:01 +0100

    puretest experiments

--- failgood/jvm/test/failgood/experiments/pure/PureHelpers.kt
+++ failgood/jvm/test/failgood/experiments/pure/PureHelpers.kt
@@ -1,9 +1,27 @@
 package failgood.experiments.pure
 
-fun <GivenType> context(
+fun <ContextGiven> root(
     name: String,
-    given: suspend () -> GivenType,
-    children: List<Node<GivenType>>
+    given: suspend GivenDSL<Unit>.() -> ContextGiven,
+    vararg children: Node<ContextGiven>
+) = Context(name, given, children.asList())
+
+fun <ParentGiven, ContextGiven> context(
+    name: String,
+    given: suspend GivenDSL<ParentGiven>.() -> ContextGiven,
+    vararg children: Node<ContextGiven>
+) = Context(name, given, children.asList())
+
+fun <ContextGiven> root(
+    name: String,
+    given: suspend GivenDSL<Unit>.() -> ContextGiven,
+    children: List<Node<ContextGiven>>
+) = Context(name, given, children)
+
+fun <ParentGiven, ContextGiven> context(
+    name: String,
+    given: suspend GivenDSL<ParentGiven>.() -> ContextGiven,
+    children: List<Node<ContextGiven>>
 ) = Context(name, given, children)
 
 fun <GivenType> test(name: String, function: suspend GivenType.() -> Unit) = Test(name, function)
@@ -13,14 +31,12 @@ sealed interface Node<out GivenType>
 data class Test<GivenType>(val name: String, val function: suspend GivenType.() -> Unit) :
     Node<GivenType>
 
-data class Context<GivenType>(
+data class Context<ParentGiven, ContextGiven>(
     val name: String,
-    val given: suspend () -> GivenType,
-    val children: List<Node<GivenType>>
-) : Node<Nothing>
+    val given: suspend GivenDSL<ParentGiven>.() -> ContextGiven,
+    val children: List<Node<ContextGiven>>
+) : Node<ParentGiven>
 
-fun <GivenType> context(
-    name: String,
-    given: suspend () -> GivenType,
-    vararg children: Node<GivenType>
-) = Context(name, given, children.asList())
+interface GivenDSL<GivenType> {
+    val given: GivenType
+}

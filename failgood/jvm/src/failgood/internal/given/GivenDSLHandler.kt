86faed69b54ed506a77851f523cd3818b017f1bd
Christoph Sturm <me@christophsturm.com>
2023-12-03 21:07:16 +0100

    implement multi level parent lookup

--- failgood/jvm/src/failgood/internal/given/GivenDSLHandler.kt
+++ failgood/jvm/src/failgood/internal/given/GivenDSLHandler.kt
@@ -3,23 +3,22 @@ package failgood.internal.given
 import failgood.dsl.GivenDSL
 import failgood.dsl.GivenLambda
 
-class GivenDSLHandler<ParentType>(private val givens: List<GivenLambda<*, *>> = listOf()) :
-    GivenDSL<ParentType> {
+class GivenDSLHandler<ParentType>(
+    private val given: GivenLambda<*, *> = {},
+    private val parent: GivenDSLHandler<*>? = null
+) : GivenDSL<ParentType> {
 
     override suspend fun given(): ParentType {
-        val givenDSL: GivenDSL<*> = SimpleGivenDSL(givens[0])
-        @Suppress("UNCHECKED_CAST") return givenDSL.(givens.last())() as ParentType
+        val result =
+            if (parent == null) Unit
+            else {
+                val g = given
+                parent.g()
+            }
+        @Suppress("UNCHECKED_CAST") return result as ParentType
     }
 
     fun <GivenType> add(given: GivenLambda<ParentType, GivenType>): GivenDSLHandler<GivenType> {
-        @Suppress("UNCHECKED_CAST")
-        val newGivens: List<GivenLambda<*, *>> = givens.plus(given as GivenLambda<*, *>)
-        return GivenDSLHandler(newGivens)
-    }
-}
-
-class SimpleGivenDSL<Type>(val lambda: GivenLambda<*, Type>) : GivenDSL<Type> {
-    override suspend fun given(): Type {
-        return lambda()
+        @Suppress("UNCHECKED_CAST") return GivenDSLHandler(given as GivenLambda<*, *>, this)
     }
 }

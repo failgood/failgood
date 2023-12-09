b65eb75ec6079524c3cf2efb8d7435e3b942e938
Christoph Sturm <me@christophsturm.com>
2023-12-09 20:40:25 +0100

    use helper methods in test

--- failgood/test/failgood/ObjectContextProviderTest.kt
+++ failgood/test/failgood/ObjectContextProviderTest.kt
@@ -5,15 +5,22 @@ import failgood.docs.ContextListExample
 import failgood.docs.testContextsOnTopLevelExampleClassName
 import failgood.experiments.TestWithDependencies
 import failgood.fixtures.PrivateContextFixture
+import kotlin.reflect.KClass
 import kotlin.test.assertNotNull
 import strikt.api.expectThat
 import strikt.assertions.*
 
 @Test
-class ObjectContextProviderTest {
+object ObjectContextProviderTest {
+    @Suppress("DEPRECATION")
+    private fun getContexts(kClass: KClass<*>) = ObjectContextProvider(kClass).getContexts()
+
+    @Suppress("DEPRECATION")
+    private fun getContexts(kClass: Class<*>) = ObjectContextProvider(kClass).getContexts()
+
     val context = describe {
-        it("provides a context from an class in a kotlin class (MyTest::class.java)") {
-            expectThat(ObjectContextProvider(ClassTestContextExample::class).getContexts())
+        it("provides a context from an class in a kotlin class (MyTest::class)") {
+            expectThat(getContexts(ClassTestContextExample::class))
                 .map { it.context.name }
                 .containsExactlyInAnyOrder(
                     "test context defined in a kotlin class",
@@ -22,44 +29,33 @@ class ObjectContextProviderTest {
                 )
         }
         it("provides a context from an object in a java class (MyTest::class.java)") {
-            expectThat(ObjectContextProvider(TestFinderTest::class.java).getContexts())
-                .single()
-                .isA<RootContext>()
-                .and { get { context.name }.isEqualTo("test finder") }
+            expectThat(getContexts(TestFinderTest::class.java)).single().isA<RootContext>().and {
+                get { context.name }.isEqualTo("test finder")
+            }
         }
         it("provides a context from an object in a kotlin class (MyTest::class)") {
-            expectThat(ObjectContextProvider(TestFinderTest::class).getContexts())
-                .single()
-                .isA<RootContext>()
-                .and { get { context.name }.isEqualTo("test finder") }
+            expectThat(getContexts(TestFinderTest::class)).single().isA<RootContext>().and {
+                get { context.name }.isEqualTo("test finder")
+            }
         }
         it("provides a list of contexts from an object in a kotlin class (MyTest::class)") {
-            expectThat(ObjectContextProvider(ContextListExample::class).getContexts())
-                .hasSize(2)
-                .all { isA<RootContext>() }
+            expectThat(getContexts(ContextListExample::class)).hasSize(2).all { isA<RootContext>() }
         }
         it("provides a top level context from a kotlin class") {
             val classLoader = ObjectContextProviderTest::class.java.classLoader
             val clazz = classLoader.loadClass(testContextsOnTopLevelExampleClassName)
-            expectThat(ObjectContextProvider(clazz.kotlin).getContexts())
-                .single()
-                .isA<RootContext>()
-                .and { get { context.name }.isEqualTo("test context declared on top level") }
+            expectThat(getContexts(clazz.kotlin)).single().isA<RootContext>().and {
+                get { context.name }.isEqualTo("test context declared on top level")
+            }
         }
         it("handles weird contexts defined in private vals gracefully") {
-            assert(
-                ObjectContextProvider(PrivateContextFixture::class)
-                    .getContexts()
-                    .map { it.context.name }
-                    .size == 2
-            )
+            assert(getContexts(PrivateContextFixture::class).map { it.context.name }.size == 2)
         }
 
         describe("correcting source info") {
             it("corrects the root context source info if its not coming from the loaded class") {
                 val contexts =
-                    ObjectContextProvider(TestClassThatUsesUtilityMethodToCreateTestContexts::class)
-                        .getContexts()
+                    getContexts(TestClassThatUsesUtilityMethodToCreateTestContexts::class)
                 expectThat(contexts).hasSize(2).all {
                     get { sourceInfo } and
                         {
@@ -74,7 +70,7 @@ class ObjectContextProviderTest {
                 }
             }
             it("does not touch the source info if it comes from the loaded class") {
-                val contexts = ObjectContextProvider(OrdinaryTestClass::class).getContexts()
+                val contexts = getContexts(OrdinaryTestClass::class)
                 expectThat(contexts).hasSize(2).all {
                     get { sourceInfo }
                         .and {
@@ -86,11 +82,7 @@ class ObjectContextProviderTest {
         }
         describe("Error handling") {
             it("throws when a class contains no contexts") {
-                expectThat(
-                        kotlin.runCatching {
-                            ObjectContextProvider(ContainsNoTests::class.java).getContexts()
-                        }
-                    )
+                expectThat(kotlin.runCatching { getContexts(ContainsNoTests::class.java) })
                     .isFailure()
                     .isA<ErrorLoadingContextsFromClass>()
                     .and {
@@ -104,9 +96,7 @@ class ObjectContextProviderTest {
                     it(
                         "wraps exceptions that happen at class instantiation: ${kClass1.simpleName}"
                     ) {
-                        expectThat(
-                                kotlin.runCatching { ObjectContextProvider(kClass1).getContexts() }
-                            )
+                        expectThat(kotlin.runCatching { getContexts(kClass1) })
                             .isFailure()
                             .isA<ErrorLoadingContextsFromClass>()
                             .and {
@@ -119,22 +109,14 @@ class ObjectContextProviderTest {
             it("gives a helpful error when a class could not be instantiated") {
                 val kClass = ClassWithConstructorParameter::class
                 val exception =
-                    assertNotNull(
-                        kotlin
-                            .runCatching { ObjectContextProvider(kClass).getContexts() }
-                            .exceptionOrNull()
-                    )
+                    assertNotNull(kotlin.runCatching { getContexts(kClass) }.exceptionOrNull())
                 val message = assertNotNull(exception.message)
                 assert(message.contains(kClass.simpleName!!)) { exception.stackTraceToString() }
             }
             it("gives a helpful error when a context method has parameters") {
                 val kClass = ClassWithContextMethodWithParameter::class
                 val exception =
-                    assertNotNull(
-                        kotlin
-                            .runCatching { ObjectContextProvider(kClass).getContexts() }
-                            .exceptionOrNull()
-                    )
+                    assertNotNull(kotlin.runCatching { getContexts(kClass) }.exceptionOrNull())
                 val message = assertNotNull(exception.message)
                 assert(
                     message ==
@@ -146,11 +128,7 @@ class ObjectContextProviderTest {
             it("returns useful error message for private classes") {
                 val jClass = javaClass.classLoader.loadClass("failgood.problematic.PrivateClass")
                 val exception =
-                    assertNotNull(
-                        kotlin
-                            .runCatching { ObjectContextProvider(jClass).getContexts() }
-                            .exceptionOrNull()
-                    )
+                    assertNotNull(kotlin.runCatching { getContexts(jClass) }.exceptionOrNull())
                 val message = assertNotNull(exception.message)
                 assert(
                     message ==

929218a4a97b6153e8bf80e99a582378658de18c
Christoph Sturm <me@christophsturm.com>
2023-12-23 16:41:54 +0100

    add some deprecated typaliases to ensure 0.8 compatibility

--- failgood/src/failgood/Deprecated.kt
+++ failgood/src/failgood/Deprecated.kt
@@ -1,4 +1,8 @@
+@file:Suppress("unused")
+
 package failgood
+
+import failgood.dsl.ContextFunctionWithGiven
 
 @Deprecated(
     message = "moved to failgood.dsl",
@@ -11,3 +15,31 @@ typealias ContextDSL<T> = failgood.dsl.ContextDSL<T>
     replaceWith = ReplaceWith("ResourcesDSL", "failgood.dsl.ResourcesDSL")
 )
 typealias ResourcesDSL = failgood.dsl.ResourcesDSL
+
+@Deprecated(
+    message = "renamed",
+    replaceWith = ReplaceWith("TestCollection<Unit>")
+)
+typealias RootContext = TestCollection<Unit>
+@Deprecated(
+    message = "renamed",
+    replaceWith = ReplaceWith("TestCollection<Given>")
+)
+typealias RootContextWithGiven<Given> = TestCollection<Given>
+
+@Deprecated(
+    message = "renamed",
+    replaceWith = ReplaceWith("TestCollection(name, ignored, order, isolation, sourceInfo, addClassName, given, function)")
+)
+fun <RootGiven> RootContextWithGiven(
+    name: String = "root",
+    ignored: Ignored? = null,
+    order: Int = 0,
+    isolation: Boolean = true,
+    sourceInfo: SourceInfo = callerSourceInfo(),
+    addClassName: Boolean = false,
+    given: (suspend () -> RootGiven),
+    function: ContextFunctionWithGiven<RootGiven>
+) =
+    TestCollection(name, ignored, order, isolation, sourceInfo, addClassName, given, function)
+

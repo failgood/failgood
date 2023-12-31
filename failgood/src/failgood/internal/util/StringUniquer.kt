7fbf4c8ca99dd449e86eb8fe3f21e4ea81bf7deb
Christoph Sturm <me@christophsturm.com>
2023-12-31 15:31:25 +0100

    StringUniquerTest

--- failgood/src/failgood/internal/util/StringUniquer.kt
+++ failgood/src/failgood/internal/util/StringUniquer.kt
@@ -3,11 +3,11 @@ package failgood.internal.util
 internal class StringUniquer {
     private val used = mutableSetOf<String>()
 
-    /** this adds a number to a string to make it unique */
-    fun makeUnique(path: String): String {
-        return if (used.add(path)) path
+    /** this adds a number to a string to make it unique within this instance */
+    fun makeUnique(string: String): String {
+        return if (used.add(string)) string
         else {
-            (1..Int.MAX_VALUE).asSequence().map { "$path-$it" }.first { used.add(it) }
+            (1..Int.MAX_VALUE).asSequence().map { "$string-$it" }.first { used.add(it) }
         }
     }
 }

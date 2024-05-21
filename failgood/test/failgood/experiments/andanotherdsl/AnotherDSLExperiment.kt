f46a9a81c0a0858d8869588245ef721c14ec3b29
Christoph Sturm <me@christophsturm.com>
2024-05-21 21:15:05 +0200

    add a comment how this could be implemented

--- failgood/test/failgood/experiments/andanotherdsl/AnotherDSLExperiment.kt
+++ failgood/test/failgood/experiments/andanotherdsl/AnotherDSLExperiment.kt
@@ -4,8 +4,13 @@ package failgood.experiments.andanotherdsl
 
 import kotlin.reflect.KProperty
 
-/*
- * with this test DSL we can discover tests upfront very fast without creating dependencies or executing tests
+/**
+ * with this test DSL we can discover tests upfront very fast without creating dependencies or executing tests.
+ * To get all test names we just execute the test collection lambda, without executing the beforeEach lambdas
+ * The for each text we execute just the path to this test (like the [failgood.internal.SingleTestExecutor] does)
+ *
+ * Or another possibility would be to just compile a list of all the beforeEach and test lambdas and then execute those for each test
+ * but then the references to the dependencies would become more complex and need to resolve to different values depending on the test
  */
 class AnotherDSLExperiment {
     val tests = testCollection("collection name") {
@@ -35,7 +40,6 @@ class ContextDependency(myDependency: MyOtherDependency) {
     fun doStuff() {
         TODO("Not yet implemented")
     }
-
 }
 
 

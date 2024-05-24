762959f97312738e8cb9c7ff56f089a4a54ff91f
Christoph Sturm <me@christophsturm.com>
2024-05-24 10:21:25 +0200

    improve flaky test a bit. currently no idea why it is flaky

--- failgood/test/failgood/functional/NameHandlingTest.kt
+++ failgood/test/failgood/functional/NameHandlingTest.kt
@@ -4,11 +4,13 @@ import failgood.Suite
 import failgood.Test
 import failgood.TestCollection
 import failgood.assert.containsExactlyInAnyOrder
+import failgood.softly.softly
 import failgood.tests
 import failgood.testsAbout
 
 @Test
 object NameHandlingTest {
+    // this mostly happens in failgood.internal.LoadResults.fixRootName
     val tests = testsAbout("name handling") {
         describe("adding test class name to test collection name") {
             it("adds the test class name to the test collection name when the test collection has no name") {
@@ -16,27 +18,32 @@ object NameHandlingTest {
                 val unnamedCollection2 = tests {}
                 val results = Suite(
                     listOf(
-                        unnamedCollection1.withNewClassName("className1"),
-                        unnamedCollection2.withNewClassName("className2")
+                        unnamedCollection1.withClassName("className1"),
+                        unnamedCollection2.withClassName("className2")
                     )
                 ).run(silent = true)
-                assert(results.contexts.map { it.name }.containsExactlyInAnyOrder("className1", "className2"))
-                assert(results.contexts.map { it.displayName }.containsExactlyInAnyOrder("className1", "className2"))
+                softly {
+                    assert(results.contexts.map { it.name }.containsExactlyInAnyOrder("className1", "className2"))
+                    assert(results.contexts.map { it.displayName }.containsExactlyInAnyOrder("className1", "className2"))
+
+                }
             }
         }
         describe("duplicate test collection names") {
             it("makes duplicate test collection names unique") {
                 val results = Suite(
-                    listOf(testsAbout("duplicate name") {}.withNewClassName("test"),
-                        testsAbout("duplicate name") {}.withNewClassName("test"))
+                    listOf(testsAbout("duplicate name") {}.withClassName("test"),
+                        testsAbout("duplicate name") {}.withClassName("test"))
                 ).run(silent = true)
-                assert(results.contexts.map { it.name }.containsExactlyInAnyOrder("duplicate name", "duplicate name-1"))
-                assert(results.contexts.map { it.displayName }.containsExactlyInAnyOrder("test: duplicate name", "test: duplicate name-1"))
+                softly {
+                    assert(results.contexts.map { it.name }.containsExactlyInAnyOrder("duplicate name", "duplicate name-1"))
+                    assert(results.contexts.map { it.displayName }.containsExactlyInAnyOrder("test: duplicate name", "test: duplicate name-1"))
+                }
             }
         }
 
     }
-    private fun TestCollection<*>.withNewClassName(className: String): TestCollection<*> {
+    private fun TestCollection<*>.withClassName(className: String): TestCollection<*> {
         return this.copy(
             rootContext = rootContext.copy(
                 sourceInfo = rootContext.sourceInfo!!.copy(

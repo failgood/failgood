3dc759c567230ffa02f52a4e8e8d5b0081fcf7c4
Christoph Sturm <me@christophsturm.com>
2024-05-26 10:18:51 +0200

    temporarily bump the timeout again to make ci pass

--- failgood/test/failgood/util/RetryForTests.kt
+++ failgood/test/failgood/util/RetryForTests.kt
@@ -24,13 +24,15 @@ class RetryForTests {
         it("retries for given time and returns result when it eventually succeeds") {
             var calls = 2
             // setting the retry timeout really high to avoid random test failures under load
+            // TODO: decrease this again after improving test throttling.
             val result =
-                retryFor(20.seconds) {
+                retryFor(60.seconds) {
                     if (calls-- > 0) throw RuntimeException("error!")
                     "result"
                 }
             assert(result == "result")
         }
+        // I'm not really sure if that's really what we want, does it really have to retry at least once?
         it("retries at least once") {
             var calls = 1
             val result =

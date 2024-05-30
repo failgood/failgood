bc4574f7eafb46426f59d19b997e8e7de33dc6a0
Christoph Sturm <me@christophsturm.com>
2024-05-30 21:40:28 +0200

    show when we lose the mdc

--- failgood/test/failgood/functional/LoggingTest.kt
+++ failgood/test/failgood/functional/LoggingTest.kt
@@ -2,7 +2,10 @@ package failgood.functional
 
 import failgood.Test
 import failgood.tests
+import org.junit.jupiter.api.Assertions.assertNull
 import org.slf4j.MDC
+import java.util.concurrent.CompletableFuture
+import kotlin.concurrent.thread
 import kotlin.test.assertNotNull
 
 
@@ -14,6 +17,14 @@ class LoggingTest {
                 val mdc = assertNotNull(MDC.get("test"))
                 assert(mdc.endsWith("> logging MDC > contains the test name"))
                 assert(mdc.startsWith("LoggingTest"))
+            }
+            it("loses the mdc when a new thread is started") {
+                val cf = CompletableFuture<String>()
+                thread {
+                    cf.complete(MDC.get("test"))
+                }.join()
+
+                assertNull(cf.get())
             }
         }
     }

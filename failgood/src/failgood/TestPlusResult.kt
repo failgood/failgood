8d7aad7eecdc8943057c0a4343c1936f71decaf4
Christoph Sturm <me@christophsturm.com>
2024-10-03 18:33:54 +0200

    make TestPlusResult.prettyPrint a receiver method

--- failgood/src/failgood/TestPlusResult.kt
+++ failgood/src/failgood/TestPlusResult.kt
@@ -7,18 +7,18 @@ data class TestPlusResult(val test: TestDescription, val result: TestResult) {
     val isSkipped: Boolean = result is Skipped
     val isFailure: Boolean = result is Failure
     val isSuccess: Boolean = result is Success
+}
 
-    fun prettyPrint(): String {
-        val testDescription = test.niceString()
-        return when (result) {
-            is Failure -> {
-                val exceptionInfo =
-                    ExceptionPrettyPrinter(result.failure, test.sourceInfo).prettyPrint()
+internal fun TestPlusResult.prettyPrint(): String {
+    val testDescription = test.niceString()
+    return when (result) {
+        is Failure -> {
+            val exceptionInfo =
+                ExceptionPrettyPrinter(result.failure, test.sourceInfo).prettyPrint()
 
-                "$testDescription:${Colors.RED} failed${Colors.RESET} with $exceptionInfo.\ntest: ${test.sourceInfo}"
-            }
-            is Success -> "$testDescription passed"
-            is Skipped -> "$testDescription skipped"
+            "$testDescription:${Colors.RED} failed${Colors.RESET} with $exceptionInfo.\ntest: ${test.sourceInfo}"
         }
+        is Success -> "$testDescription passed"
+        is Skipped -> "$testDescription skipped"
     }
 }

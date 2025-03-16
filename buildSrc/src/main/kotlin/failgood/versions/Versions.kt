1e53b8eb0d11fb10107891ca36aba17ff0f5efc9
Christoph Sturm <me@christophsturm.com>
2025-03-16 15:36:21 +0100

    remove unused confusing version constant

--- buildSrc/src/main/kotlin/failgood/versions/Versions.kt
+++ buildSrc/src/main/kotlin/failgood/versions/Versions.kt
@@ -1,6 +1,5 @@
 package failgood.versions
 
-const val kotlinVersion = "2.0.20"
 const val coroutinesVersion = "1.10.1"
 const val striktVersion = "0.35.1"
 const val junitPlatformVersion = "1.12.0"

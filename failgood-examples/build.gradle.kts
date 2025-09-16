920b76f60d16b991525e308132cfda4384789648
Christoph Sturm <me@christophsturm.com>
2025-09-16 18:53:32 +0200

    update pitest in example project

--- failgood-examples/build.gradle.kts
+++ failgood-examples/build.gradle.kts
@@ -31,7 +31,7 @@ plugins.withId("info.solidsoft.pitest") {
         avoidCallsTo.set(setOf("kotlin.jvm.internal", "kotlin.Result"))
         targetClasses.set(setOf("failgood.examples.*")) // by default "${project.group}.*"
         targetTests.set(setOf("failgood.examples.*Test", "failgood.examples.**.*Test"))
-        pitestVersion.set("1.20.1")
+        pitestVersion.set("1.20.3")
         threads.set(
             System.getenv("PITEST_THREADS")?.toInt() ?: Runtime.getRuntime().availableProcessors())
         outputFormats.set(setOf("XML", "HTML"))
